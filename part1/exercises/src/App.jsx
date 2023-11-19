96ed6f61ede5295453ae3a554c415048241e890b
LeedsByExample <leeda.minkin@yahoo.com>
2023-11-18 19:17:39 -0600

    Readded HobbyLinks import

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -1,15 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD:part1/exercises/src/App.js
-import MovieList from './components/MovieList.js';
-import ChoresList from './components/ChoresList.js';
-import BookList from './components/BookList.js';
-import HobbyLinks from './components/HobbyLinks.js';
-=======
 import MovieList from './components/MovieList.jsx';
 import ChoresList from './components/ChoresList.jsx';
 import BookList from './components/BookList.jsx';
->>>>>>> 23376bea5f18ea7f602882c0c6b0416ab34cc74d:part1/exercises/src/App.jsx
+import HobbyLinks from './components/HobbyLinks.js';
 
 function App() {
   return (
